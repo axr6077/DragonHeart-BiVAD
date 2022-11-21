4f8949e89ef2d4f5bc17539479d28d75e337df74
Ayush Rout <axr6077@rit.edu>
2022-11-21 00:17:17 -0500

    gpio utility functions

---
+++ accessor.cpp
@@ -0,0 +1,35 @@
+#include<accessor.h>
+
+bool Accessor::isAvailable_GPIO[] = {0};
+bool Accessor::isAvailable_ANALOG[] = {0};
+bool Accessor::isLoaded_CAPE = 0;
+
+Accessor::Accessor()
+{
+
+}
+
+
+void Accessor::gpio_makeAvailable(unsigned int pin_Num)
+{
+  isAvailable_GPIO[pin_Num] = 0;
+}
+
+
+void Accessor::gpio_makeUnAvailable(unsigned int pin_Num)
+{
+  isAvailable_GPIO[pin_Num] = 1;
+}
+
+
+
+bool Accessor::gpio_isAvailable(unsigned int pin_Num)
+{
+
+  if(isAvailable_GPIO[pin_Num])
+    return 0;
+
+  else
+    return 1;
+}
+
