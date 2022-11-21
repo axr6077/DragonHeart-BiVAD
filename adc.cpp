6a8da409c13ad5d10561907a90d1da1380a0fd5c
Ayush Rout <axr6077@rit.edu>
2022-11-21 00:58:46 -0500

    testing program for adc

---
+++ adc.cpp
@@ -0,0 +1,17 @@
+#define ANALOG_SYSFS "/sys/devices/bone_capemgr.*/slots"
+
+#include<stdlib.h>
+#include<iostream>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdio.h>
+#include<cstdlib>
+#include<analog_access.h>
+
+using namespace std;
+
+int main(void) {
+// todo
+
+}
