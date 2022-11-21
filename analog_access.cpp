e78dafa160fa78400276be32fd28e12d3d34d973
Ayush Rout <axr6077@rit.edu>
2022-11-21 00:41:34 -0500

    load analog cape and initialize analog pins

--- analog_access.cpp
+++ analog_access.cpp
@@ -1,2 +1,26 @@
 #define LOAD_CAPE "echo BB-ADC > /sys/devices/bone_capemgr.8/slots"
 #define ANALOG_SYSPATH "/sys/devices/ocp.2/helper.14/AIN"
+
+#include <analog_access.h>
+
+/*
+Load analog cape
+*/
+
+Analog_In::Analog_In()
+{
+  if(isLoaded_CAPE != 1)
+    {
+      cout<<"Attaching\n";
+      system("echo BB-ADC > /sys/devices/bone_capemgr.8/slots");
+      cout<<"Attaching Finished\n";
+      is_attached = 0;
+      isLoaded_CAPE = 1;
+    }
+  else
+    {
+      cout<<"Analog Cape already loaded\n";
+    }
+}
+
+
