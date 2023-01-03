84c6d6bd397952eeead3750749babe567ed117db
Ayush Rout <axr6077@rit.edu>
2023-01-02 22:08:00 -0800

    Update pid.cpp

--- adc-tests/src/pid.cpp
+++ adc-tests/src/pid.cpp
@@ -9,3 +9,59 @@ constexpr double K_D = 0.01;
 // ADC and DAC device files
 constexpr char ADC_DEVICE[] = "/dev/adc0";
 constexpr char DAC_DEVICE[] = "/dev/dac0";
+int main() {
+    std::ifstream adc_file(ADC_DEVICE);
+    std::ofstream dac_file(DAC_DEVICE);
+    if (!adc_file || !dac_file) {
+        std::cerr << "Unable to open ADC or DAC device file" << std::endl;
+        return 1;
+    }
+
+    int adc_value;
+    int dac_value;
+    double error;
+    double p_term;
+    double i_term = 0.0;
+    double d_term;
+    double control_signal;
+    double setpoint = 50.0;  // Setpoint for the control system
+
+    // Continuously run the PID control loop
+    while (true) {
+        // Read the ADC value
+        adc_file >> adc_value;
+        if (adc_file.fail()) {
+            std::cerr << "Unable to read from ADC device file" << std::endl;
+            break;
+        }
+
+        // Calculate the error
+        error = setpoint - adc_value;
+
+        // Calculate the proportional term
+        p_term = K_P * error;
+	
+	// Update the integral term
+        i_term += error;
+
+        // Calculate the derivative term
+        d_term = K_D * (error - prev_error);
+        prev_error = error;
+
+        // Calculate the control signal
+        control_signal = p_term + K_I * i_term + d_term;
+
+        // Output the control signal to the DAC
+        dac_value = (int)(control_signal + 0.5);
+        dac_file << dac_value << std::endl;
+        if (dac_file.fail()) {
+            std::cerr << "Unable to write to DAC device file" << std::endl;
+            break;
+        }
+
+        // Sleep for a short time to allow the control system to stabilize
+        usleep(1000);
+    }
+
+    return 0;
+}
